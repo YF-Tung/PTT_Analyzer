fcc29814971efbe82f0abd074a440fe81a0d42fd
yftung <r02921044@ntu.edu.tw>
2016-05-09 19:31:51 +0800

    TF-IDF (not done)

--- PTT_Analyzer.py
+++ PTT_Analyzer.py
@@ -2,89 +2,79 @@
 import sqlite3
 import operator
 import os
+import numpy
 
 con = sqlite3.connect('PTT_Parser.db')
 c = con.cursor()
 
-# authorDict:
-#   key (string): author
-#   value (dict): dict:
-#	key (string): board
-#	value (int): count
+# boardDict:
+#   key (string): board
+#   value (dict): 3-element list
+#       dict:
+#	    key (string): author
+#	    value (int): count
+#       list: TF, then TF*IDF
+#       int: articleNumber
+boardDict = {}
 
-authorDict = {}
+authorSet = set()
 
-for row in c.execute('SELECT * FROM BAP'):
+for row in c.execute('SELECT * FROM BAP LIMIT 1000'):
     board = row[1]
     author = row[2]
-    dic = authorDict.setdefault(author, {})
-    dic[board] = dic.get(board, 0) + 1
+    tpl = boardDict.setdefault(board, [{}, [], 0])
+    dic = tpl[0]
+    dic[author] = dic.get(author, 0) + 1
+    authorSet.add(author)
+    tpl[2] += 1
 
-#for k,v in authorDict.items():
-#    print(k, v)
+authorList = list(authorSet)
+authorNum = len(authorList)
 
-# corrDict: dict
-#   key(pair of string): (brd1, brd2) with brd1 < brd2
-#   value: count
+#TF
+for brd in boardDict:
+    dic = boardDict[brd][0]
+    TF = boardDict[brd][1]
+    artNum = boardDict[brd][2]
+    for i in range(authorNum):
+        TF.append( float(dic.get(authorList[i], 0)) / artNum )
 
-corrDict = {}
-for author, boardDict in authorDict.items():
-    boardList = boardDict.items()
-    brdNum = len(boardList)
-    if brdNum == 1: continue
-    for i in range(0, brdNum-1):
-	for j in range(i+1, brdNum):
-	    brdA = boardList[i][0]
-	    brdB = boardList[j][0]
-	    addWeight = min(boardList[i][1] , boardList[j][1])
-	    if brdA > brdB:
-		brdB, brdA = (brdA, brdB)
-	    corrDict[(brdA, brdB)] = corrDict.get((brdA, brdB),0) + addWeight
+#IDF
+IDF = []
+boardNumF = float(len(boardDict))
+for aut in authorList:
+    cnt = 0
+    for tpl in boardDict.values():
+        if aut in tpl[0]: #author wrote some in the board
+            cnt += 1
+    IDF.append( boardNumF / cnt )
 
-#for k, v in corrDict.items():
-#    print(k, v)
-sortedCorr = sorted(corrDict.items(), key=operator.itemgetter(1))
-for elem in sortedCorr:
-    print(elem)
+#TF -> TF*IDF
+for tpl in boardDict.values():
+    TF = tpl[1]
+    result = TF
+    for i in range(len(result)):
+        result[i] *= IDF[i]
+    nrm = numpy.linalg.norm(result)
+    result = [ x / nrm for x in result ]
 
-def addToRecDict(ba, bb, cnt, recDict):
-    dic = recDict.setdefault(ba, {})
-    dic[bb] = dic.get(bb, 0) + cnt
-    dic = recDict.setdefault(bb, {})
-    dic[ba] = dic.get(ba, 0) + cnt
 
-recDict = {}
-for brdP, count in corrDict.items():
-    addToRecDict(brdP[0], brdP[1], count, recDict)
+# Example: 'travel' to others
+print('travel' in boardDict)
 
-#for k, v in recDict.items():
-#    print(k)
-#    print(v)
+myDic = {} #board to cosine
+for brd in boardDict:
+    if brd == 'travel': continue
+    myDic[brd] = numpy.dot(boardDict[brd][1], boardDict['travel'][1])
+print(myDic)
 
-# Compute average indegree for every node
-nBrd = len(recDict.items())
-avgDeg = {}
-for k, v in recDict.items():
-    s = 0
-    for i in v.values():
-	s += i
-    avgDeg[k] = float(s) / nBrd
+myList = []
+for k,v in myDic.items():
+    myList.append( (v,k) )
+myList.sort(reverse=True)
+for tpl in myList:
+    if not tpl[0] == 0:
+        print(tpl[1], '\t', tpl[0])
 
-if not os.path.exists('data'):
-    os.makedirs('data')
 
-for brd in recDict:
-    outputList = []
-    for k, v in recDict[brd].items():
-	outputList.append( (v/(1+avgDeg[k]), v, k.encode('utf8')) )
-    outputList.sort(reverse=True)
-
-    fh = open('data/' + brd.encode('utf8') + '.data', 'w')
-    fh.write('board\tdegree\tnordegree\n')
-    for tup in outputList:
-	fh.write(tup[2]+'\t'+str(tup[1])+'\t'+str(tup[0])+'\n')
-
-#    for k,v in recDict[brd].items():
-#	fh.write(k.encode('utf8') + '\t' + str(v) +
-#	'\t' + str(v / avgDeg[k]) + '\n')
 
